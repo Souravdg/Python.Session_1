f1e2f481954627bf9ce7917f978718fb30611ae4
Souravdg <45494334+Souravdg@users.noreply.github.com>
2018-12-12 12:26:29 -0500

    Add files via upload

---
+++ Random Arrays.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import numpy as np
+np.random.seed(0)
+x1 = np.random.randint(10, size=6)
+x2 = np.random.randint(10, size=(3, 4))
+x3 = np.random.randint(10, size=(3, 4, 5))
+
+
+# In[4]:
+
+
+print (x1)
+
+
+# In[5]:
+
+
+print(x2)
+
+
+# In[6]:
+
+
+print (x3)
+
